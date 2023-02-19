b86d83e8fc1f57095b640a9642c6c983e106a28e
borissofft <kolegata86@abv.bg>
2023-02-19 23:46:45 +0200

    "Books" REST Service initial commit

---
+++ books-js-client/src/main/java/bg/softuni/booksjsclient/BooksJsClientApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.booksjsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksJsClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksJsClientApplication.class, args);
+    }
+
+}
---
+++ books-js-client/src/test/java/bg/softuni/booksjsclient/BooksJsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.booksjsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksJsClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
