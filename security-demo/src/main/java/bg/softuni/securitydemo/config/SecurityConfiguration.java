9b13ffb573b4abe792eab5711c7fcbe90d030fb9
borissofft <kolegata86@abv.bg>
2023-02-27 20:39:30 +0200

    Added new features for login. Need to implement register.

--- security-demo/src/main/java/bg/softuni/securitydemo/config/SecurityConfiguration.java
+++ security-demo/src/main/java/bg/softuni/securitydemo/config/SecurityConfiguration.java
@@ -21,18 +21,29 @@ public class SecurityConfiguration {
     http.
         // defines which pages will be authorized
         authorizeHttpRequests().
+            // allow access to all static files (images, CSS, js)
           requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll().
+            // the URL-s below are available for all users - logged in and anonymous
           requestMatchers("/", "/users/login", "/users/register", "/users/login-error").permitAll().
+            // only for moderators
           requestMatchers("/pages/moderators").hasRole(UserRoleEnum.MODERATOR.name()).
+            // only for admins
           requestMatchers("/pages/admins").hasRole(UserRoleEnum.ADMIN.name()).
         anyRequest().authenticated().
           and().
+            // configure login with HTML form
           formLogin().
             loginPage("/users/login").
+            // the names of the username, password input fields in the custom login form
             usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY).
             passwordParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY).
-        defaultSuccessUrl("/", true).
-        failureForwardUrl("/users/login-error");
+            // where do we go after login
+            defaultSuccessUrl("/"). // use true argument if you always want to go there, otherwise go to previous page
+            failureForwardUrl("/users/login-error"). // here need to implement @PostMapping("/users/login-error")
+          and().logout(). // configure logout
+            logoutUrl("/users/logout").
+            logoutSuccessUrl("/"). // go to home page after logout
+            invalidateHttpSession(true);
 
     return http.build();
   }
