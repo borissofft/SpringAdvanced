9c67bb729b588d91f3cf9bb43bfae5c3d1a8fa23
borissofft <kolegata86@abv.bg>
2023-03-06 20:48:43 +0200

    Cloudinary2 demo initial commit

---
+++ cloudinary2/src/main/java/bg/softuni/cloudinary2/Cloudinary2Application.java
@@ -0,0 +1,13 @@
+package bg.softuni.cloudinary2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cloudinary2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cloudinary2Application.class, args);
+    }
+
+}
