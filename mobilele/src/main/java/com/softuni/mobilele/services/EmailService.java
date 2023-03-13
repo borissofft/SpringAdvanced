55462bf3fbc7b1602711ca490399032256af2734
borissofft <kolegata86@abv.bg>
2023-03-13 12:15:32 +0200

    Email demo. Using Scheduling we can monitor the quantity of goods in the warehouse for example and send email if it drops below some value with our Email demo.

---
+++ mobilele/src/main/java/com/softuni/mobilele/services/EmailService.java
@@ -0,0 +1,57 @@
+package com.softuni.mobilele.services;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+import java.util.Locale;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+    private final TemplateEngine templateEngine; // Here we will use TemplateEngine, not thymeleaf to parse the template
+
+    public EmailService(JavaMailSender javaMailSender,
+                        TemplateEngine templateEngine) {
+        this.javaMailSender = javaMailSender;
+        this.templateEngine = templateEngine;
+    }
+
+    public void sendRegistrationEmail(
+            String userEmail,
+            String userName
+    ) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+
+        try {
+            mimeMessageHelper.setFrom("mobilele@example.com");
+            mimeMessageHelper.setTo(userEmail);
+            //TODO: i18n - (means internationalisation)
+            mimeMessageHelper.setSubject("Welcome to Mobilele!");
+            mimeMessageHelper.setText(generateEmailText(userName), true);
+
+            javaMailSender.send(mimeMessageHelper.getMimeMessage());
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private String generateEmailText(String userName) {
+        Context ctx = new Context();
+        ctx.setLocale(Locale.getDefault());
+        ctx.setVariable("userName", userName);
+
+        return templateEngine.process("email/registration", ctx);
+    }
+
+}
---
+++ mobilele/src/main/java/com/softuni/mobilele/services/TempEmailTest.java
@@ -0,0 +1,22 @@
+package com.softuni.mobilele.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TempEmailTest implements CommandLineRunner {
+
+    private  final EmailService emailService;
+
+    @Autowired
+    public TempEmailTest(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        emailService.sendRegistrationEmail("lucho@example.com", "Lucho");
+    }
+}
