345d53193197dc5363663b95b8d6db02e5ba0987
borissofft <kolegata86@abv.bg>
2023-03-29 23:59:39 +0300

    Offer controller is completed. Can delete offer. Idea for searching by some criteria.

--- mobilele/src/main/java/com/softuni/mobilele/web/OfferController.java
+++ mobilele/src/main/java/com/softuni/mobilele/web/OfferController.java
@@ -1,11 +1,17 @@
 package com.softuni.mobilele.web;
 
 import com.softuni.mobilele.services.OfferService;
+
 import java.util.UUID;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,32 +19,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/offers")
 public class OfferController {
+    private final OfferService offerService;
 
-  private final OfferService offerService;
+    public OfferController(OfferService offerService) {
+        this.offerService = offerService;
+    }
 
-  public OfferController(OfferService offerService) {
+    @PreAuthorize("@offerService.isOwner(#userDetails, #id)")
+    @DeleteMapping("/{id}")
+    public String deleteOffer(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @PathVariable("id") UUID id) {
 
-    this.offerService = offerService;
-  }
+        offerService.deleteOfferByUUID(id);
 
-  @GetMapping("/{id}")
-  public String getOfferById(@PathVariable("id") UUID offerUUID) {
-    //todo
-    return "details";
-  }
+        return "redirect:/offers/all";
+    }
 
-  @GetMapping("/all")
-  public String getAllOffers(Model model,
-      @PageableDefault(
-          sort = "offerId",
-          size = 3
-      ) Pageable pageable) {
+    @GetMapping("/{id}")
+    public String getOfferById(@PathVariable("id") UUID offerUUID,
+                               @AuthenticationPrincipal UserDetails userDetails,
+                               Model model) {
 
-    var allOffersPage = offerService.getAllOffers(pageable);
+        model.addAttribute("offer", offerService.getOfferById(offerUUID));
+        model.addAttribute("canDelete", offerService.isOwner(userDetails, offerUUID));
 
-    model.addAttribute("offers", allOffersPage);
+        return "details";
+    }
 
-    return "offers";
-  }
+    @GetMapping("/all")
+    public String getAllOffers(Model model,
+                               @PageableDefault(
+                                       sort = "offerId",
+                                       size = 3
+                               ) Pageable pageable) {
+
+        var allOffersPage = offerService.getAllOffers(pageable);
+
+        model.addAttribute("offers", allOffersPage);
+
+        return "offers";
+    }
 
 }
